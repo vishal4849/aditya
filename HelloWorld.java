a914a7dc5b523fc4ad1a4052383278af0306b569
vishal4849 <vj605226@gmail.com>
2024-12-29 14:50:48 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,5 +2,6 @@ public class HelloWorld {
     public static void main(String[] args) {
         // Printing "Hello, World!" to the console
         System.out.println("Hello, World!");
+        System.out.println("Aditya");
     }
 }
